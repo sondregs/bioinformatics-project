626b3b690572054eda4e62752e8db84c4dd90cbf
Mathias Wahl <ma10asw@gmail.com>
2020-04-11 12:58:56 +0200

    add gunicorn as reqirement, correct app path in flaskcon

--- api/.flaskenv
+++ api/.flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=app.py
+FLASK_APP=api/app.py
 FLASK_ENV=development
--- api/app.py
+++ api/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__, static_folder="../build", static_url_path="/")
 app.config["DEBUG"] = True
 
 from api import routes
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
